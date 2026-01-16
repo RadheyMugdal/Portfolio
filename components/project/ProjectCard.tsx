5b7a113cc03e4d76049a7de63e4cbc07b7bb210b
Radhey Mugdal <mugdalradhey@gmail.com>
2026-01-16 22:35:30 +0530

    refactor: reorganize ProjectCard layout by moving action buttons to a new section for improved clarity and maintainability

--- components/project/ProjectCard.tsx
+++ components/project/ProjectCard.tsx
@@ -38,7 +38,12 @@ const ProjectCard = ({
   return (
     <Card key={id} className=" pt-3 group overflow-hidden">
       <div className=" w-[calc(100%-24px)] rounded-md overflow-hidden mx-auto h-[200px]  relative">
-        <Image src={thumbnailUrl} fill alt="mockup" className=" group-hover:scale-110 transition-transform duration-400 ease-in-out object-cover" />
+        <Image
+          src={thumbnailUrl}
+          fill
+          alt="mockup"
+          className=" group-hover:scale-110 transition-transform duration-400 ease-in-out object-cover"
+        />
       </div>
       <div className=" flex flex-col flex-1 justify-between  gap-4">
         <CardHeader className=" px-4 space-y-1">
@@ -65,24 +70,24 @@ const ProjectCard = ({
               ))}
             </div>
           </div>
+          <div className="mt-4 grid grid-cols-2 gap-2">
+            <Link href={githubUrl} target="_blank">
+              <Button variant={"secondary"} className=" w-full">
+                <Github />
+                Github
+              </Button>
+            </Link>
+            <Link href={webUrl} target="_blank">
+              <Button className="group/button w-full">
+                <Globe />
+                Visit
+                <ArrowUpRight className="group-hover/button:translate-x-0.5 group-hover/button:-translate-y-0.5 transition-transform duration-300" />
+              </Button>
+            </Link>
+          </div>
         </CardContent>
-        <CardFooter className=" px-4 grid grid-cols-2 gap-2">
-          <Link href={githubUrl} target="_blank">
-            <Button variant={"secondary"} className=" w-full" >
-              <Github />
-              Github
-            </Button>
-          </Link>
-          <Link href={webUrl} target="_blank">
-            <Button className="group/button w-full"  >
-              <Globe />
-              Visit
-              <ArrowUpRight className="group-hover/button:translate-x-0.5 group-hover/button:-translate-y-0.5 transition-transform duration-300" />
-            </Button>
-          </Link>
-        </CardFooter>
       </div>
-    </Card >
+    </Card>
   );
 };
 
