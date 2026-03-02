0d0b7e044461e4570b5b53f207a052f960791c4c
Radhey Mugdal <mugdalradhey@gmail.com>
2026-03-02 22:49:54 +0530

    feat: define runtime environment for email sending function

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -3,6 +3,8 @@ import { NextRequest } from 'next/server';
 import EmailTemplate from '../../../components/contact/EmailTemplate';
 import { Resend } from 'resend';
 
+export const runtime = "nodejs"
+
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(req: NextRequest) {
